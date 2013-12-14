af90fbe46e8c7d84f19d71e681ef83ab7f57365d
Arie <arieyayank@gmail.com>
2013-12-14 12:56:15 +0700

    Create melayang.js

---
+++ melayang.js
@@ -0,0 +1,21 @@
+
+if (screen.width >= 600) {
+$(function() {
+    var $sidebar   = $(".sidebar"), 
+        $window    = $(window),
+        offset     = $sidebar.offset(),
+        topPadding = 15;
+
+    $window.scroll(function() {
+        if ($window.scrollTop() > offset.top) {
+            $sidebar.stop().animate({
+                marginTop: $window.scrollTop() - offset.top + topPadding
+            });
+        } else {
+            $sidebar.stop().animate({
+                marginTop: 20
+            });
+        }
+    });
+}); 
+}
